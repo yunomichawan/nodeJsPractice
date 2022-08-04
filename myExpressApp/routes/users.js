85ebd4ba97b28edfe82d45ce9c98504e3b1b859d
yunomichawan <ddlkgjbdnm@gmail.com>
2022-08-04 16:18:56 +0900

    ローカルでのアクセス先を追記 ルートのアクセス方法を確認

--- myExpressApp/routes/users.js
+++ myExpressApp/routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+/* 次のようにアクセスする「http://localhost:3000/users/test」 */
+router.get('/test', function (req, res, next) {
+  res.send('respond with a test');
 });
 
 module.exports = router;
